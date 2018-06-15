6108dc996e01598af4c33e0993314a42fb6d676a
Oganesyan Artur <arturris@mail.ru>
2018-06-15 01:18:36 -0500

    More Button with logic implementation

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import classes from './buildControl.css';
+import classes from './BuildControl.css';
 
-const buildControl = (props) => )
+const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}> {props.label} </div>
-        <button className={classes.Less}>>Less</button>
-        <button className={classes.More}>>More</button>
+        <button className={classes.Less}>Less</button>
+        <button onClick={props.added} className={classes.More}>More</button>
     </div>   
 )
 
