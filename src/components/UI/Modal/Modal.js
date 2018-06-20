82d13ebc882603e07c17195b9ae0419982ad3973
Oganesyan Artur <arturris@mail.ru>
2018-06-20 09:52:58 -0500

    Modal.js  is statefull and folder structure changing V1.0.0

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css'
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal =(props) => (
-    <Aux>
-        <Backdrop clicked={props.modalClosed} show={props.show} />
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate(){
+        console.log('[Modal] componentDidUpdate')
+    }
+
+    render(){
+        return(
+            <Aux>
+        <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
         <div 
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
             }}
             >
-            {props.children}
+            {this.props.children}
         </div>
     </Aux>
-);
+        )
+    }  
+};
 
-export default modal;
+export default Modal;
